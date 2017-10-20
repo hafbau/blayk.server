6777275cecfe7eb5083a9a1b22e7e7aa119c5bb5
Hafiz Suara <hafizsuara@Hafizs-MBP.hitronhub.home>
2017-10-20 13:47:06 -0600

    updated locate to default to xpath

--- business_logics/locate.js
+++ business_logics/locate.js
@@ -10,11 +10,20 @@ module.exports = function locate({ target: { type, value }, driver }) {
     try {
         value = value.toLowerCase();
         switch (type) {
+            case 'id':
+                return driver.wait(until.elementLocated(By.id(value)), TIMEOUT);
+
+            case 'xpath':
+                return driver.wait(until.elementLocated(By.xpath(value)), TIMEOUT);
+
+            case 'linkText':
+                return driver.wait(until.elementLocated(By.linkText(value)), TIMEOUT);
+
             case 'css':
                 return driver.wait(until.elementLocated(By.css(value)), TIMEOUT)
             
             default:
-                const XPATH = `//*[contains(${lower('text()')}, '${value}') or ${lower('@placeholder')}='${value}' or ${lower('@name')
+                const XPATH = `//*[contains(${lower('@class')}, '${value}') or contains(${lower('text()')}, '${value}') or ${lower('@placeholder')}='${value}' or ${lower('@name')
         }='${value}' or ${lower('@id')}='${value}']`
                 return driver.wait(until.elementLocated(By.xpath(XPATH)), TIMEOUT)
 
